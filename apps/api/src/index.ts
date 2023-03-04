114d15240f73b6bdd1c5d871559660e00669c2a9
alirezazandi <alirezazandi7800@gmail.com>
2023-03-04 22:09:16 +0330

    add web socket

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,18 @@
-console.log('sadas');
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('user conected');
+});
+
+httpServer.listen(8000);
